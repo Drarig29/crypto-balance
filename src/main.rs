a99568243b2f195230003ef8854928be124b37e8
Corentin Girard <corentingirard.dev@gmail.com>
2021-04-07 17:32:22 +0200

    Make actual working request

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,37 @@
 #[macro_use]
 extern crate rocket;
 extern crate reqwest;
+extern crate hmac;
+extern crate sha2;
+extern crate hex;
 
 use env::VarError;
 use std::io;
 use std::env;
+use std::time::SystemTime;
 use std::path::{Path, PathBuf};
-use std::collections::HashMap;
 
 use rocket::response::NamedFile;
 use rocket::response::content;
 
-use reqwest::{Client, Method};
+use reqwest::blocking::{Client};
+
+use sha2::Sha256;
+use hmac::{Hmac, Mac, NewMac};
+
+// Create alias for HMAC-SHA256
+type HmacSha256 = Hmac<Sha256>;
 
 struct BinanceAuth {
     key: String,
     secret: String,
 }
+
+const API_BASE_URL: &str = "https://api.binance.com/sapi/v1/accountSnapshot";
+
+const ACCOUNT_TYPE_SPOT: &str = "SPOT";
+const ACCOUNT_TYPE_MARGIN: &str = "MARGIN";
+const ACCOUNT_TYPE_FUTURES: &str = "FUTURES";
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
@@ -41,12 +56,28 @@ fn get_env_vars() -> Result<BinanceAuth, VarError> {
     })
 }
 
+// TODO: make a struct with accountType, startTime, endTime and limit as properties
+
 fn get_binance_snapshots(auth: BinanceAuth) -> Result<String, reqwest::Error> {
-    println!("Key: {}, Secret: {}", auth.key, auth.secret);
-    
-    match reqwest::blocking::get("https://api.binance.com/sapi/v1/accountSnapshot")?.text() {
-        Ok(res) => return Ok(res),
-        Err(e) => return Err(e),
+    let client = Client::new();
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+
+    let params = format!("type=SPOT&limit={}&timestamp={}", 5, now);
+    println!("params: {}", params);
+
+    let mut mac = HmacSha256::new_varkey(auth.secret.as_bytes()).unwrap();
+    mac.update(params.as_bytes());
+
+    let hash_message = mac.finalize().into_bytes();
+    let signature = hex::encode(&hash_message);
+    println!("signature: {}", signature);
+
+    let url = format!("{}?{}&signature={}", API_BASE_URL, params, signature);
+    let res = client.get(url).header("X-MBX-APIKEY", auth.key).send()?;
+
+    match res.text() {
+        Ok(res) => Ok(res),
+        Err(e) => Err(e),
     }
 }
 
