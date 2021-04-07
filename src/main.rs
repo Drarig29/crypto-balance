afef09b0d11a22da22126e13275539a7726f392f
Corentin Girard <corentingirard.dev@gmail.com>
2021-04-07 10:38:38 +0200

    Get binance api credentials

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,49 @@
 #[macro_use]
 extern crate rocket;
 
+use env::VarError;
 use std::io;
+use std::env;
 use std::path::{Path, PathBuf};
 
 use rocket::response::NamedFile;
 use rocket::response::content;
+
+struct BinanceAuth {
+    key: String,
+    secret: String,
+}
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
 }
 
+fn get_env_vars() -> Result<BinanceAuth, VarError> {
+    let key = match env::var("BINANCE_API_KEY") {
+        Ok(res) => res,
+        Err(e) => return Err(e),
+    };
+    
+    let secret = match env::var("BINANCE_API_SECRET") {
+        Ok(res) => res,
+        Err(e) => return Err(e),
+    };
+
+    Ok(BinanceAuth {
+        key,
+        secret,
+    })
+}
+
 #[get("/api")]
-fn api() -> content::Json<&'static str> {
-    content::Json("{ test: 'hello' }")
+fn api() -> content::Json<String> {
+    let env_variables = match get_env_vars() {
+        Ok(res) => res,
+        Err(err) => return content::Json(err.to_string())
+    };
+
+    content::Json(format!("{{ key: {key}, secret: {secret} }}", key=env_variables.key, secret=env_variables.secret))
 }
 
 #[get("/<file..>")]
