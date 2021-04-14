b1f77a97219c2cd213a37c7619e14a854ed6ef96
Corentin Girard <corentingirard.dev@gmail.com>
2021-04-14 15:52:20 +0200

    Add colors to the pie (doughnut) chart

--- frontend/DougnutChart.jsx
+++ frontend/DougnutChart.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Rainbow from 'rainbowvis.js';
+
+import { Legend } from 'recharts';
+import { Tooltip } from 'recharts';
 import { PieChart } from 'recharts';
 import { Pie } from 'recharts';
+import { Cell } from 'recharts';
 
 export default function ({ data }) {
     const assets = (Object.keys(data || {})).filter(key => key !== "Total as BTC" && key !== "time");
@@ -11,9 +16,19 @@ export default function ({ data }) {
     const values = assets.map(asset => ({ name: asset, value: data[asset] }));
     console.log({ values });
 
+    const rainbow = new Rainbow();
+    rainbow.setNumberRange(0, values.length + 1);
+
     return (
         <PieChart width={300} height={300} margin={{ left: 50, right: 50 }}>
-            <Pie data={values} nameKey="name" dataKey="value" cx="50%" cy="50%" fill="#82ca9d" label></Pie>
+            <Pie data={values} nameKey="name" dataKey="value" cx="50%" cy="50%" innerRadius={50} >
+                {values.map((_, index) => {
+                    const color = `#${rainbow.colorAt(index)}`;
+                    return <Cell key={index} stroke={color} fill={color} fillOpacity={0.6} />
+                })}
+            </Pie>
+            <Tooltip />
+            <Legend />
         </PieChart>
     )
 }
