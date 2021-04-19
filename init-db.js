0d323c92b19c09bcb740894d4ab9b3e1b26e9b2a
Corentin Girard <corentingirard.dev@gmail.com>
2021-04-19 14:12:17 +0200

    Better mongodb indexes

--- init-db.js
+++ init-db.js
@@ -1,2 +1,2 @@
-db.snapshots.createIndex({ time: 1 });
-db.history.createIndex({ time: 1 });
+db.snapshots.createIndex({ time: 1 }, { unique: true });
+db.history.createIndex({ time: 1, asset: 1 }, { unique: true });
