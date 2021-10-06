70df9b52e8afb3764859f23cce79bb997b109930
Corentin Girard <corentingirard.dev@gmail.com>
2021-10-06 15:29:50 +0200

    Detect incorrect password in login screen

--- frontend/components/LoginScreen.jsx
+++ frontend/components/LoginScreen.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { Context } from '..';
+import { sendRequest } from '../helpers';
 
-const Form = ({ onSubmit }) => {
+const Form = ({ error, onSubmit }) => {
     const passwordRef = useRef();
 
     const handleSubmit = e => {
@@ -16,6 +17,7 @@ const Form = ({ onSubmit }) => {
                 <img src='assets/password.svg'></img>
                 <input type='password' autoComplete='password' ref={passwordRef} placeholder='Password' />
             </div>
+            {error && <p className='danger'>{error}</p>}
             <button type='submit'>Submit</button>
         </form>
     );
@@ -23,9 +25,21 @@ const Form = ({ onSubmit }) => {
 
 export const LoginScreen = ({ children }) => {
     const [context, setContext] = useContext(Context);
+    const [message, setMessage] = useState(null);
 
     if (context.password)
-        return children
+        return children;
 
-    return <Form onSubmit={password => setContext({ ...context, password })}></Form>
+    const handlePassword = async (password) => {
+        const response = await sendRequest('/auth', { password });
+
+        if (response.status === 200) {
+            setMessage(null);
+            setContext({ ...context, password });
+        } else {
+            setMessage(response.body);
+        }
+    }
+
+    return <Form error={message} onSubmit={handlePassword}></Form>
 }
