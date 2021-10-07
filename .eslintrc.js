b98cc07a5eb2e777c1cfc60c2a5ff196a81000e4
Corentin Girard <corentingirard.dev@gmail.com>
2021-10-07 14:25:10 +0200

    Update babel and esbuild

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:react/recommended'
     ],
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     plugins: [
         'jsx-a11y'
     ],
